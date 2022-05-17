989e8236abf9106a743126323f3858e2e24876c2
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2022-05-17 14:37:27 -0400

    testing updates

--- hyrax/db/seeds.rb
+++ hyrax/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(
+  :email => 'changeme@mail.wvu.edu',
+  :password => 'password',
+  :password_confirmation => 'password'
+)
+user.save!
+
+admin = Role.create(name: "admin")
+admin.users << User.find_by_user_key( "changeme@mail.wvu.edu" )
+admin.save
