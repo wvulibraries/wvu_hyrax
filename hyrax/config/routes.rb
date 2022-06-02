a53ddb7acf356738565474e7296cfc704acd0bbc
Jesse Griffis <jagriffis@mail.wvu.edu>
2022-06-02 10:00:36 -0400

    cancan admin abilities, sidekiq, and turbolinks implemented

--- hyrax/config/routes.rb
+++ hyrax/config/routes.rb
@@ -31,12 +31,12 @@ Rails.application.routes.draw do
     end
   end
 
-  # require 'sidekiq/web'
+  require 'sidekiq/web'
   # require 'sidekiq/cron/web'
-  mount Sidekiq::Web => '/sidekiq'
+  # mount Sidekiq::Web => '/sidekiq'
   # config/routes.rb
-  # authenticate :user, lambda { |u| u.admin? } do
-  #   mount Sidekiq::Web => '/sidekiq'
-  # end  
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
