72979263ba74d8038cdd67cd5a2a01d521793866
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2022-06-07 18:24:59 -0400

    updated tests

--- hyrax/spec/models/audio_spec.rb
+++ hyrax/spec/models/audio_spec.rb
@@ -3,7 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Audio do
-  it "has tests" do
-    skip "Add your tests here"
+  # factory
+  let(:audio) { FactoryBot.create :audio }
+
+  # shared examples 
+  context 'shared examples' do
+    it_behaves_like 'a valid factory' # factorybot
   end
 end
