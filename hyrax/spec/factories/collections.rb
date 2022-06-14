3a4c20962b02d8dd7176a7de8458666f1278bf90
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2022-06-14 12:49:15 -0400

    updated code

--- hyrax/spec/factories/collections.rb
+++ hyrax/spec/factories/collections.rb
@@ -2,7 +2,14 @@ FactoryBot.define do
   factory :collection do
     depositor { Faker::Internet.email }
     title { [Faker::Lorem.sentence] }
-    collection_type_gid { "gid://wvu-hyrax/Hyrax::CollectionType/1" } # normally auto generated on save of collection
     description { [Faker::Lorem.paragraph] }
+
+    factory :user_collection do
+      collection_type_gid { "gid://wvu-hyrax/Hyrax::CollectionType/1" } # User Collection
+    end
+
+    factory :user_collection do
+      collection_type_gid { "gid://wvu-hyrax/Hyrax::CollectionType/2" } # Admin Set
+    end    
   end
 end
