8c163fc7ddfae151818e7ed1c7bca265cd1ad632
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2022-09-02 09:22:39 -0400

    update for fits and java

--- hyrax/config/initializers/hyrax.rb
+++ hyrax/config/initializers/hyrax.rb
@@ -73,7 +73,7 @@ Hyrax.config do |config|
   # config.redis_namespace = "hyrax"
 
   # Path to the file characterization tool
-  # config.fits_path = "fits.sh"
+  config.fits_path = "/home/fits/fits.sh"
 
   # Path to the file derivatives creation tool
   # config.libreoffice_path = "soffice"
