511d030eabe3a254cea656e0c431502c2c8cf923
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2022-09-14 17:51:05 -0400

    updated setup.sh for new path

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@ echo "Preparing Database"
 bin/rails db:create
 
 # if schema.rb exists load schema else run the migrations
-FILE=/home/hyrax/db/schema.rb
+FILE=/home/wvu_hyrax/db/schema.rb
 if [test -f "$FILE"]; then
     bin/rails db:schema:load
 else
