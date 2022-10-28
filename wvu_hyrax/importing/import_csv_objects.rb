c1d4ff42f321a93d166393e9d7d2ddf374675e1d
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2022-10-28 11:06:27 -0400

    current updated manual import of mfcs export

--- wvu_hyrax/importing/import_csv_objects.rb
+++ wvu_hyrax/importing/import_csv_objects.rb
@@ -15,33 +15,43 @@ class Import
 
   private
 
-  # def rights_statement_id(str)
-  #   arry = [
-  #     [ id: "http://rightsstatements.org/vocab/InC/1.0/", term: "In Copyright", active: true ],
-  #     [ id: "http://rightsstatements.org/vocab/InC-OW-EU/1.0/", term: "In Copyright - EU Orphan Work", active: true ],
-  #     [ id: "http://rightsstatements.org/vocab/InC-EDU/1.0/", term: "In Copyright - Educational Use Permitted", active: true ],
-  #     [ id: "http://rightsstatements.org/vocab/InC-NC/1.0/", term: "In Copyright - Non-Commercial Use Permitted", active: true ],
-  #     [ id: "http://rightsstatements.org/vocab/InC-RUU/1.0/", term: "In Copyright - Rights-holder(s) Unlocatable or Unidentifiable" active: true],
-  #     [ id: "http://rightsstatements.org/vocab/NoC-CR/1.0/", term: "No Copyright - Contractual Restrictions", active: true],
-  #     [ id: "http://rightsstatements.org/vocab/NoC-NC/1.0/", term: "No Copyright - Non-Commercial Use Only", active: true],
-  #     [ id: "http://rightsstatements.org/vocab/NoC-OKLR/1.0/", term: "No Copyright - Other Known Legal Restrictions", active: true],
-  #     [ id: "http://rightsstatements.org/vocab/NoC-US/1.0/", term: "No Copyright - United States", active: true],
-  #     [ id: "http://rightsstatements.org/vocab/CNE/1.0", term: "Copyright Not Evaluated", active: true],
-  #     [ id: "http://rightsstatements.org/vocab/UND/1.0/", term: "Copyright Undetermined", active: true],
-  #     [ id: "http://rightsstatements.org/vocab/NKC/1.0/", term: "No Known Copyright", active: true], 
-  #     [ id: "https://rightsstatements.org/page/InC/1.0/", term: "Active - In Copyright", active: false],
-  #     [ id: "http://creativecommons.org/publicdomain/mark/1.0/", term: "Expired - Public Domain (Creative Commons)", active: true],
-  #     [ id: "https://rightsstatements.org/page/InC-OW-EU/1.0/", term: "Orphan - In Copyright - EU Orphan Work", active: true],
-  #     [ id: "https://rightsstatements.org/page/UND/1.0/", term: "Uncertain - Copyright Undetermined", active: false]
-  #   ]
-  # end
+  def rights_statement_uri(str)
+    arry = [
+      { id: "http://rightsstatements.org/vocab/InC/1.0/", term: "In Copyright", active: true },
+      { id: "http://rightsstatements.org/vocab/InC-OW-EU/1.0/", term: "In Copyright - EU Orphan Work", active: true },
+      { id: "http://rightsstatements.org/vocab/InC-EDU/1.0/", term: "In Copyright - Educational Use Permitted", active: true },
+      { id: "http://rightsstatements.org/vocab/InC-NC/1.0/", term: "In Copyright - Non-Commercial Use Permitted", active: true },
+      { id: "http://rightsstatements.org/vocab/InC-RUU/1.0/", term: "In Copyright - Rights-holder(s) Unlocatable or Unidentifiable", active: true},
+      { id: "http://rightsstatements.org/vocab/NoC-CR/1.0/", term: "No Copyright - Contractual Restrictions", active: true},
+      { id: "http://rightsstatements.org/vocab/NoC-NC/1.0/", term: "No Copyright - Non-Commercial Use Only", active: true},
+      { id: "http://rightsstatements.org/vocab/NoC-OKLR/1.0/", term: "No Copyright - Other Known Legal Restrictions", active: true},
+      { id: "http://rightsstatements.org/vocab/NoC-US/1.0/", term: "No Copyright - United States", active: true},
+      { id: "http://rightsstatements.org/vocab/CNE/1.0", term: "Copyright Not Evaluated", active: true},
+      { id: "http://rightsstatements.org/vocab/UND/1.0/", term: "Copyright Undetermined", active: true},
+      { id: "http://rightsstatements.org/vocab/NKC/1.0/", term: "No Known Copyright", active: true}, 
+      { id: "https://rightsstatements.org/page/InC/1.0/", term: "Active - In Copyright", active: false},
+      { id: "http://creativecommons.org/publicdomain/mark/1.0/", term: "Expired - Public Domain (Creative Commons)", active: true},
+      { id: "https://rightsstatements.org/page/InC-OW-EU/1.0/", term: "Orphan - In Copyright - EU Orphan Work", active: true},
+      { id: "https://rightsstatements.org/page/UND/1.0/", term: "Uncertain - Copyright Undetermined", active: false}
+    ]
 
-  # split string using ||| as the delimiter then sort the array
-  def split_string(str)
-    # return nil if string length is 0
-    return nil if str.to_s.mb_chars.length == 0
-    arr = str.split('|||')
-    arr.sort
+    # find the correct term id and return it
+    arry.each do |item|
+      # if the term matches the string then return the id
+      if item[:term] == str
+        return [] << item[:id]
+      end
+    end
+
+    # return empty array if no match is found
+    return []
+  end
+
+  # split string using the delimiter and return an arraybin
+  def string_to_array(str, delimiter = '|||')
+    # return empty array if string length is 0
+    return [] if str.to_s.mb_chars.length == 0
+    str.split(delimiter)
   end
 
   # remove ascii character references for the actual character
@@ -69,31 +79,30 @@ class Import
     # create hash for the item
     hash = {
       # source_identifier: row['source_identifier'],      
-      identifier: [] << row['source_identifier'],
+      identifier: [] << row['identifier'],
       depositor: "tam0013@mail.wvu.edu",
-      # id: row['id'],
       title: [] << row['title'],
       date_uploaded: row['date_created'],
-      # date_modified: Time.at(@hash['modifiedTime'].to_i).to_datetime,
       institution: row['institution'],
       subtype: [] << row['subtype'],
       extent: (row['extent'] || ""),
       resource_type: [] << row['resource_type'],
-      creator: self.split_string(row['creator']),
-      contributor: self.split_string(row['contributor']),
+      creator: self.string_to_array(row['creator']),
+      contributor: self.string_to_array(row['contributor']),
       description: [] << self.remove_special_chars(row['description']),
-      # needs to be updated to read what is in the csv and convert it to the format below
-      # should be a yml somewhere that has the actual values to compare to
-      rights_statement: [] << "http://rightsstatements.org/vocab/InC-EDU/1.0/", 
+      rights_statement: rights_statement_uri(row['rights_statement']), 
       date_created: [] << row['date_created'],
-      subject: self.split_string(row['subject']),
-      language: self.split_string(row['language']),
+      subject: self.string_to_array(row['subject']),
+      language: self.string_to_array(row['language']),
       source: [] << row['source'],
       visibility: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC,
       admin_set_id: "admin_set/default"
     }
+
+    # create the item
     item = BasicWork.new(hash)
     item.save
+
     # return the id of the new item
     item.id
   end
@@ -124,9 +133,9 @@ class Import
 
   def import_objects
     # set collection identifier
-    source = 'folklife'
+    source = '4224'
     # set data directory
-    csv_text = File.read("/home/wvu_hyrax/imports/#{source}/export/bulkrax/#{source}-data.csv")
+    csv_text = File.read("/home/wvu_hyrax/imports/folklife/export/bulkrax/folklife-data-test.csv")
 
     csv = CSV.parse(csv_text, :headers => true)
     csv.each do |row|
@@ -147,7 +156,7 @@ class Import
         item_id = create_item(row)
         set_item_collection(item_id, source)
         # add files to item
-        add_file_to_item(item_id, source, row['file'])
+        add_file_to_item(item_id, 'folklife', row['file'])
       end
     end
   end
