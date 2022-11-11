74514f917b98971cc3a05a1c5228bb9c212523ce
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2022-11-11 13:57:47 -0500

    adjusted hyrax jobs and sidekiq queues.

---
+++ wvu_hyrax/app/jobs/content_event_job.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+# A generic job for sending events about repository objects to a user and their followers.
+#
+# @attr [String] repo_object the object event is specified for
+#
+class ContentEventJob < EventJob
+  attr_reader :repo_object
+  def perform(repo_object, depositor)
+    # if depositor is nil, then the object was created by a batch process and we don't need to notify anyone
+    #return unless depositor
+    depositor = User.find_by_user_key(repo_object.depositor) if depositor.nil?
+
+    @repo_object = repo_object
+    
+    super(depositor)
+    log_event(repo_object)
+  end
+
+  # Log the event to the object's stream
+  def log_event(repo_object)
+    repo_object.log_event(event)
+  end
+
+  # log the event to the users profile stream
+  def log_user_event(depositor)
+    depositor.log_profile_event(event)
+  end
+end
