e20b38289cef8b7fed325fda6022f15be7049095
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2023-02-09 14:20:02 -0500

    work update

--- wvu_hyrax/app/forms/hyrax/basic_work_form.rb
+++ wvu_hyrax/app/forms/hyrax/basic_work_form.rb
@@ -4,9 +4,7 @@ module Hyrax
   # Generated form for Work
   class BasicWorkForm < Hyrax::Forms::WorkForm
     self.model_class = ::BasicWork
-    self.terms = [:title, :creator, :date, :description, :subject, :place, :extent, :language, :resource_type, :format, :collection, :source, :identifier, :institution, :rights_statement, :recommeneded_citation, :notes]
-
-    self.required_fields -= [:creator]
-    self.required_fields += [:collection, :identifier, :institution]
+    self.terms = [:title, :creator, :date, :description, :subject, :place, :extent, :language, :resource_type, :format, :collection, :source, :identifier, :institution, :rights_statement, :administrative_notes]
+    self.required_fields = [:title, :collection, :source, :identifier, :institution, :rights_statement]
   end
 end
