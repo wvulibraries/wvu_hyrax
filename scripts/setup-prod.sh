4885337969864fba505ccf033918d6d061fdd1a9
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2023-03-15 16:45:36 -0400

    updated script

---
+++ scripts/setup-prod.sh
@@ -0,0 +1,14 @@
+echo "Preparing Database"
+bin/rails db:drop db:create
+
+# if schema.rb exists load schema else run the migrations
+FILE="/home/wvu_hyrax/db/schema.rb"
+if [ -e $FILE ]; then
+    bin/rails db:schema:load
+else
+    bin/rails db:migrate
+fi
+
+bin/rails db:seed
+bin/rails assets:precompile
+
