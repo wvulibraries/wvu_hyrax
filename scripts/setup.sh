ee68502b49a0e60112435158e11d653c8f9aac88
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2023-06-09 16:02:47 -0400

    current updates - moved to ruby 2.7.8 updated gems

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,12 @@
 echo "Preparing Database"
-bin/rails db:create
-bin/rails db:migrate
+bin/rails db:drop db:create
+
+# if schema.rb exists load schema else run the migrations
+FILE="/home/wvu_hyrax/db/schema.rb"
+if [ -e $FILE ]; then
+    bin/rails db:schema:load
+else
+    bin/rails db:migrate
+fi
+
 bin/rails db:seed
